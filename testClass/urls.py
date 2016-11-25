23517c48121293d98abb2495412c17de302ef1aa
LABELNET <1406046087@qq.com>
2016-11-25 16:26:05 +0800

    【EDIT】实现简单的详情

--- testClass/urls.py
+++ testClass/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import FoodList
+from .views import FoodList, FoodDetail
 
 urlpatterns = [
-    url(r'^index$', FoodList.as_view())
+    url(r'^index$', FoodList.as_view()),
+    url(r'^detail/(?P<pk>[0-9]+)$', FoodDetail.as_view()),
 ]
--- testClass/views.py
+++ testClass/views.py
@@ -1,8 +1,29 @@
 # Create your views here.
+from django.views.generic import DetailView
 from django.views.generic import ListView
 
 from testClass.models import Food
 
 
 class FoodList(ListView):
+    """
+    继承 listview ，指定model；
+    context_object_name : 自定义模板上的 object名字,默认的是 object_list
+    example:
+      {% for food in foods %}
+    """
     model = Food
+    context_object_name = 'foods'
+
+
+class FoodDetail(DetailView):
+    """
+    详情页
+    """
+    model = Food
+    slug_field = 'pk'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(FoodDetail, self).get_context_data(**kwargs)
+    #     context['food'] = Food.objects.all()
+    #     return context
