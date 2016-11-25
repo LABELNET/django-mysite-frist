9c007ed534e760e04fe870602f6273401a8e088c
LABELNET <1406046087@qq.com>
2016-11-25 16:59:33 +0800

    【EDIT】detailview和listview实现

--- testClass/urls.py
+++ testClass/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from .views import FoodList, FoodDetail
+from .views import FoodList, FoodDetail, FoodListDef, FoodListQuery
 
 urlpatterns = [
     url(r'^index$', FoodList.as_view()),
+    url(r'^indexdef$', FoodListDef.as_view()),
+    url(r'^indexquery$', FoodListQuery.as_view()),
     url(r'^detail/(?P<pk>[0-9]+)$', FoodDetail.as_view()),
 ]
--- testClass/views.py
+++ testClass/views.py
@@ -7,6 +7,7 @@ from testClass.models import Food
 
 class FoodList(ListView):
     """
+    1.list第一种写法
     继承 listview ，指定model；
     context_object_name : 自定义模板上的 object名字,默认的是 object_list
     example:
@@ -18,12 +19,32 @@ class FoodList(ListView):
 
 class FoodDetail(DetailView):
     """
-    详情页
+    详情页 :
+    slug_field 指定参数 pk
     """
     model = Food
     slug_field = 'pk'
+    context_object_name = 'food'
 
-    # def get_context_data(self, **kwargs):
-    #     context = super(FoodDetail, self).get_context_data(**kwargs)
-    #     context['food'] = Food.objects.all()
-    #     return context
+
+class FoodListDef(ListView):
+    """
+    2.list 第二种写法
+      context_object_name 就等于 foods_def
+    """
+    model = Food
+
+    def get_context_data(self, **kwargs):
+        context = super(FoodListDef, self).get_context_data(**kwargs)
+        context['foods_def'] = Food.objects.all()
+        return context
+
+
+class FoodListQuery(ListView):
+    """
+     3.list 第三种写法
+       需要指定 template_name
+    """
+    context_object_name = 'foods_query'
+    queryset = Food.objects.all()
+    template_name = 'testClass/food_list_query.html'
